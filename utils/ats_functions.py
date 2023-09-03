f1ef9227972b9fd30275912d75696f9d764196f2
Aisha Shaikh <aishashaikhv@gmail.com>
2023-09-03 21:42:44 +0530

    init :pronoun detection

---
+++ utils/ats_functions.py
@@ -0,0 +1,6 @@
+class Ats_Functions:
+   def __init__(self):
+    pass
+
+    def pronouns(resume):
+        pronouns=['I', 'We','Me','My','Mine','Myself','You','Your','Yours','Yourself','She','Her','Hers','Herself','He','Him','Himself','His','They','Them','Themself','Themselves','Their','Us','Our','Ourselves','Ourself','Ours','It','Itself'] 
