c091e0f36e62c4857a92f9b926fd17684bb4d19d
rrahulg <rahulgeorge1212@gmail.com>
2023-09-03 21:48:31 +0530

    init

---
+++ utils/ats_functions.py
@@ -0,0 +1,9 @@
+class Ats_function:
+    def __init__(self):
+        pass
+
+    def character(self,resume):
+        if len(self.resume)>threshold:
+            return 0 
+        else:
+            return 1
