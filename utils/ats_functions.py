09c46185979b3d6d9ab34b9d57c2fd22e7c518cf
rrahulg <rahulgeorge1212@gmail.com>
2023-09-04 18:47:16 +0530

    [ADDED] comments [UPDATED] code

--- utils/ats_functions.py
+++ utils/ats_functions.py
@@ -3,15 +3,18 @@ class Ats_functions:
     def __init__(self):
         pass
 
+
+#this func returns the value true or false 
     def resume_length(self,resume):
+        thresholdl_limit = 670  
+#This needs to be updated with the correct data
         with open(resume, "r") as json_file:
             data = json.load(json_file)
             extracted_data = [value for section in data.values() if isinstance(section, list) for dictionary in section for value in dictionary.values()]
             char = ''.join(map(str, extracted_data))
+#droppin all the special characters and whitespces
             for x in " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~":
                 char = char.replace(x,"")
-
-        print(char)
         if len(char)>670:
             return False
         else:
@@ -20,5 +23,6 @@ class Ats_functions:
 if __name__ == "__main__":
     smthn = Ats_functions()
     resume = 'temp.json'
-    value = smthn.resume_length("temp.json")
+    value = smthn.resume_length(resume)
+# 'resume' consists of the input json file 
 
