ff346083b191e7f4a02b58db0d981425b044b469
rrahulg <rahulgeorge1212@gmail.com>
2023-09-04 13:42:34 +0530

    [UPDATED] made a function of class Ats_functions to check resume length and tested with temp.json

--- utils/ats_functions.py
+++ utils/ats_functions.py
@@ -1,9 +1,24 @@
-class Ats_function:
+import json
+class Ats_functions:
     def __init__(self):
         pass
 
-    def character(self,resume):
-        if len(self.resume)>threshold:
-            return 0 
+    def resume_length(self,resume):
+        with open(resume, "r") as json_file:
+            data = json.load(json_file)
+            extracted_data = [value for section in data.values() if isinstance(section, list) for dictionary in section for value in dictionary.values()]
+            char = ''.join(map(str, extracted_data))
+            for x in " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~":
+                char = char.replace(x,"")
+
+        print(char)
+        if len(char)>670:
+            return False
         else:
-            return 1
+            return True
+
+if __name__ == "__main__":
+    smthn = Ats_functions()
+    resume = 'temp.json'
+    value = smthn.resume_length("temp.json")
+
