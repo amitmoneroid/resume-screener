201fd079a4a357e5ca60f79c469c0ccefb2be59e
Aisha Shaikh <aishashaikhv@gmail.com>
2023-09-06 00:07:25 +0530

    [FEAT] pronoun threshold function

--- utils/ats_functions.py
+++ utils/ats_functions.py
@@ -1,6 +1,19 @@
 class Ats_Functions:
-   def __init__(self):
-    pass
+    def __init__(self):
+        pass
 
-    def pronouns(resume):
-        pronouns=['I', 'We','Me','My','Mine','Myself','You','Your','Yours','Yourself','She','Her','Hers','Herself','He','Him','Himself','His','They','Them','Themself','Themselves','Their','Us','Our','Ourselves','Ourself','Ours','It','Itself'] 
+    def pronouns(self, resume):
+        pers_pron = ['I', 'We', 'Me', 'My', 'Mine', 'Myself', 'You', 'Your', 'Yours', 'Yourself', 'She', 'Her', 'Hers', 'Herself', 'He', 'Him', 'Himself', 'His', 'They', 'Them', 'Themself', 'Themselves', 'Their', 'Us', 'Our', 'Ourselves', 'Ourself', 'Ours', 'It', 'Itself']
+        lowercase = resume.lower().split(" ")
+
+        count = 0
+
+        for pronoun in pers_pron:
+            count += lowercase.count(pronoun.lower())
+
+        threshold = 5
+        if count >= threshold:
+            print("Resume rejected due to overuse of personal pronouns")
+        else:
+            print("Resume accepted!")
+
