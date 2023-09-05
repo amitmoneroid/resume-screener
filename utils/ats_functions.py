a5ccfa8ed3e74176215e42893a3edd656d635738
Saurabha Sawant <saurabha.srs75@gmail.com>
2023-09-05 20:58:43 +0530

    [FEAT] 1.Checks for Bullet point 2. Checks if Paragraph persent in Resume 3.Returns If resume is acceptible or not

--- utils/ats_functions.py
+++ utils/ats_functions.py
@@ -1,5 +1,6 @@
 import json 
 import re 
+from nltk.tokenize import word_tokenize
 
 class Ats_Functions:
     def __init__(self,path):
@@ -7,6 +8,8 @@ class Ats_Functions:
         
         #assigns data from the path by loading load_data
         self.load_data()
+        
+        
     def load_data(self):
         try: 
             #loads data into the  json.data from the Json file 
@@ -17,21 +20,50 @@ class Ats_Functions:
         except FileNotFoundError:
             print(f"File not found: {self.path}")
             self.data= None 
-    def Check_bullet_count(self, json_text):
-        #splits the long string into multiple short string which starts with optional
-        # blank <space/s> at the beginning followed by a • [alt + 0149]
-        bullet_point_pattern = r'^\s*[\\u]'
+            
+            
+            
+    def Check_bullet_count(self):
+        #splits the long string into tokens by tokenization with optional
+        # NOTe:  • [alt + 0149]
+        tokens= word_tokenize(self.path)
+        count = 0
+
+    # regex can be utilized to reduce time complexity
+        for token in tokens:
+            if token == '•':
+                count += 1
+
+        return count
         
-        matches = re.findall(bullet_point_pattern, json_text, re.MULTILINE)
+        #Checks for: If the user has used appropriate no. of bullet points. And returns an COMMENT about it
+    def Check_bullet_point(self):
+        ct=self.Check_bullet_count()
+        if ct < 3:                                      #Lower limit for bullet point                                      
+            print ("Utilize More Bullet Points")
+        elif ct > 8:                                    #Higher limit for bullet point
+            print("Too many Bullet Points used")
+        else:
+            print("Good Bullet Point Utilization")
+              
         
-        return len(matches)
-if __name__ == "__main__":
-    path = 'reccomended_course.json'  # Replace with the path to your JSON file
-    ats = Ats_Functions(path)
+    def IsParagraph(self):
+        
+        if isinstance(self.data, str):
+            # This regular expression pattern starts checking/counting the sentence after the first character of [.!?]
+            sentence_pattern = r'[^.!?]*[.!?]'
+            
+            # Split the data into sentences using the sentence_pattern. 
+            sentences = re.findall(sentence_pattern, self.data)
+            
+            # Check for paragraph: If multiple sentence then It is  paragraph
+            if len(sentences) > 1:
+                return True
+            else:
+                return False
     
-#test
-with open('reccomended_course.json') as test:
-     text=json.load(test)
-# Process a specific resume
-print(ats.Check_bullet_count(text))
-        
+    def Resume_status(self):
+        if IsParagraph():
+            print("RESUME IS NOT ACCEPTIBLE")
+        else:
+            print("RESUME IS ACCEPTIBLE")
