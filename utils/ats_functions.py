43e48c1d69b587dbfeb4eb0331c657e238f80c5b
Riddhi Joshi <riddhijoshi5900@gmail.com>
2023-09-06 22:28:43 +0530

    feat:implemented profile image detector

---
+++ utils/ats_functions.py
@@ -0,0 +1,18 @@
+import fitz
+from PIL import Image
+
+class Ats_Functions:
+    def __init__(self,file):
+        self.file = file
+
+    def check_images(self):
+        pdf = fitz.open(file)
+        page = pdf[0]
+        images = page.get_images()
+
+        if images:
+            print(f"Found {len(images)} images")
+            print("Resume not acceptable")
+        else:
+            print("No images found")
+            print("Resume acceptable")
