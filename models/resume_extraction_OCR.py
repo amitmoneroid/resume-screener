1def4eb277272b69d776e8fb4d62e77d4ef33409
Ankit <ankitxtowod@gmail.com>
2023-09-24 02:49:06 +0530

    refactor: Improved resume extraction using pdfplumber

--- models/resume_extraction_OCR.py
+++ models/resume_extraction_OCR.py
@@ -1,36 +1,33 @@
-from PyPDF2 import PdfReader
+import pdfplumber 
 import json
 
-class PDFTextExtractor:
-    def __init__(self, pdf_file):
-        self.pdf_file = pdf_file
+class ResumeExtractor:
+    def __init__(self, resume_file):
+        self.resume_file = resume_file
         self.extracted_data = {
-            "file_name": pdf_file,
-            "data": []
+            'file_name': self.resume_file,
+            'data': []
         }
+    
+    def extract_text(self):
+            
+        with pdfplumber.open(self.resume_file) as resume:
+            for page in resume.pages:
+                text = page.extract_text(x_tolerance=2)
+                if text:
+                    self.extracted_data['data'].append(text)
 
-    def extract_text_from_pdf(self):
-        reader = PdfReader(self.pdf_file)
-
-        # Looping through all pages in the PDF
-        for page in reader.pages:
-            # Extracting text from the page and appending it to the 'data' list
-            text = page.extract_text()
-            self.extracted_data['data'].append(text)
-
+        return self.extracted_data
+    
     def to_json(self):
         return json.dumps(self.extracted_data, indent=4)
 
+# Usage example:
+# resume_file_path = "../data/testing2.pdf"
+resume_file_path = "../data/Resume-TCET-FORMAT-PKS.pdf"
+resume_extractor = ResumeExtractor(resume_file_path)
+resume_extractor.extract_text()  # Extract text first
 
-pdf_file_path = '../data/Resume-TCET-FORMAT-PKS.pdf'
+json_data = resume_extractor.to_json()  # Converting to JSON
+print(json_data)
 
-
-pdf_extractor = PDFTextExtractor(pdf_file_path)
-
-# Extract text from the PDF
-pdf_extractor.extract_text_from_pdf()
-
-json_data = pdf_extractor.to_json()
-
-# Print the JSON data
-# print(json_data)
