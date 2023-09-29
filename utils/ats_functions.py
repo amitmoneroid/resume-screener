9ba9dc837a3fbe88bd63a8fce4fc5149c990d8c8
Aisha Shaikh <aishashaikhv@gmail.com>
2023-09-30 01:35:22 +0530

    Coverted to Json format

--- utils/ats_functions.py
+++ utils/ats_functions.py
@@ -1,10 +1,18 @@
-class Ats_Functions:
-    def __init__(self):
-        pass
+import json
 
-    def pronouns(self, resume):
+class Ats_Functions:
+    def __init__(self, path):
+        self.path = path
+        with open(self.path, 'r') as json_file:
+            self.data = json.load(json_file)
+
+    def pronoun(self):
         pers_pron = ['I', 'We', 'Me', 'My', 'Mine', 'Myself', 'You', 'Your', 'Yours', 'Yourself', 'She', 'Her', 'Hers', 'Herself', 'He', 'Him', 'Himself', 'His', 'They', 'Them', 'Themself', 'Themselves', 'Their', 'Us', 'Our', 'Ourselves', 'Ourself', 'Ours', 'It', 'Itself']
-        lowercase = resume.lower().split(" ")
+        
+        # Extract the text from your JSON data (adjust the key as needed)
+        text = self.data.get('text_field', '') 
+
+        lowercase = text.lower().split(" ")
 
         count = 0
 
@@ -14,6 +22,8 @@ class Ats_Functions:
         threshold = 5
         if count >= threshold:
             print("Resume rejected due to overuse of personal pronouns")
+            return False
         else:
             print("Resume accepted!")
+            return True
 
